b8fc28a1dd491bf09534819e50318af019584fae
Stefan Andjelkovic <victory20201224@gmail.com>
2022-11-08 01:41:37 +0900

    init

---
+++ main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"time"
+	"net/http"
+	"strconv"
+    "encoding/json"
+	"github.com/gin-gonic/gin"
+)
+
+type position struct {
+	Lat     float64  `json:"lat"`
+	Lon  	float64  `json:"lon"`
+}
+
+type WeatherInfo struct {
+	ID     			int  	`json:"id"`
+	Main  			string  `json:"main"`
+	Description 	string  `json:"description"`
+	Icon  			string 	`json:"icon"`
+}
+
+type SubMainInfo struct {
+	Temp     		float64	`json:"temp"`
+	FeelsLike	  	float64 `json:"feels_like"`
+	TempMin		 	float64 `json:"temp_min"`
+	TempMax  		float64 `json:"temp_max"`
+	Pressure  		int 	`json:"pressure"`
+	Humidity  		int 	`json:"humidity"`
+	SeaLevel  		int 	`json:"sea_level"`
+	GrndLevel  		int 	`json:"grnd_level"`
+}
+
+type WindInfo struct {
+	Speed  			float64 `json:"speed"`
+	Deg  			int  	`json:"deg"`
+	Gust		 	float64 `json:"gust"`
+}
+
+type CloudInfo struct {
+	All     		int  	`json:"all"`
+}
+
+type SysInfo struct {
+	ID     			int  	`json:"id"`
+	Type  			int  	`json:"type"`
+	Country 		string  `json:"country"`
+	Sunrise  		int 	`json:"sunrise"`
+	Sunset  		int 	`json:"sunset"`
+}
+
+type MainInfo struct {
+	Coord			position
+	Weather			[]WeatherInfo
+	Base			string `json:"base"`
+	Main			SubMainInfo
+	Visibility  	int 	`json:"visibility"`
+	Wind			WindInfo
+	Clouds			CloudInfo
+	Dt  			int 	`json:"dt"`
+	Sys				SysInfo
+	Timezone  		int 	`json:"timezone"`
+	Id  			int 	`json:"id"`
+	Name  			string 	`json:"name"`
+	Cod  			int 	`json:"cod"`
+}
+
+
+var myClient = &http.Client{Timeout: 10 * time.Second}
+
+func getJson(url string, target interface{}) error {
+	r, err := myClient.Get(url)
+	if err != nil {
+		return err
+	}
+	defer r.Body.Close()
+
+	return json.NewDecoder(r.Body).Decode(target)
+}
+
+func main() {
+	router := gin.Default()
+	router.POST("/get-weather", getWeatherByPosition)
+
+	router.Run("0.0.0.0:3000")
+}
+
+func getWeatherByPosition(c *gin.Context) {
+	var cp position
+
+	if err := c.BindJSON(&cp); err != nil {
+		c.IndentedJSON(http.StatusOK, gin.H{"message": err})
+		return
+	}
+
+	url := "https://api.openweathermap.org/data/2.5/weather?lat=" + strconv.FormatFloat(cp.Lat, 'f', 6, 64) + "&lon=" + strconv.FormatFloat(cp.Lon, 'f', 6, 64) +"&appid=68f2c51c10fb42b906d3364af7115828"
+
+	info := new(MainInfo)
+	getJson(url, info)
+
+	c.IndentedJSON(http.StatusOK, info)
+}
